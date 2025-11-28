d45713da50957322bede3e69d705d013d00e1358
youssef <youssefyoussef.m4@gmail.com>
2025-11-28 22:52:52 +0200

    initial commit

---
+++ queries.sql
@@ -0,0 +1,7 @@
+SELECT s.student_name FROM student AS s WHERE s.level = "SR" AND EXISTS 
+    (SELECT * FROM semester_course as sc NATURAL JOIN enrolled as e WHERE s.student_id = e.student_id AND sc.prof_id="1"); 
+
+(SELECT s.student_name, c.`name` FROM student AS s NATURAL JOIN enrolled AS e NATURAL JOIN semester_course AS sc NATURAL JOIN course AS c)
+UNION
+(SELECT s.student_name, NULL AS name FROM student AS s WHERE NOT EXISTS 
+    (SELECT 1 FROM enrolled as e WHERE e.student_id=s.student_id));
